58994cade612ff7045ebdcd48fee5c61378494eb
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2025-05-07 21:05:57 +0530

    Migrate to prisma 6 and use biome for lint (#4)

--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -1,5 +1,5 @@
 import fp from 'fastify-plugin';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/client/index.js';
 
 export default fp(async function prismaClientPlugin(app, opts) {
   const prisma = new PrismaClient(opts.dbConfig);
